7d6da3dd33bdb7617be1af0052cf14f9fd6ccbb9
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2021-12-24 20:54:24 +0300

    Init commit

---
+++ run.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+
+os.system("chmod -R 0777 /model")
+
+with open('/model/run.py', 'w') as f:
+
+    f.write('pip install /model/pyflow\n')
+    
+    Path('/model/result').mkdir(parents=True, exist_ok=True)
+    
+    videos = os.listdir("/dataset")
+
+    for video in videos:
+        f.write(f"python3 /model/eval.py --data_dir /dataset/{video} --output /model/result\n")
+
+    f.write("chmod -R 0777 /model\n")
+
+os.system("chmod -R 077 /model")
+os.system("/model/run.sh")
