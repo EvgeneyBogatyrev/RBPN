8df0ef3981e99a9f9bfd1467cf476c7f1ea2eac9
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-01-04 20:24:24 +0300

    Change interface

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 pip install /model/pyflow
-python3 /model/eval.py --data_dir /dataset/gauss --output /model/result
-python3 /model/eval.py --data_dir /dataset/bicubic --output /model/result
-chmod -R 0777 /model
+python3 /model/eval.py --data_dir /dataset/bicubic --output /results
+chmod -R 0777 /results
